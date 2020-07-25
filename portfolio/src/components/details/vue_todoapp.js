b7e84e2ec4592d54fe97105f7ed7fa32e0f0bb6f
Azusa Okamoto <curiousazunas@gmail.com>
2020-07-25 21:00:14 +0900

    成果物ページに詳細ページを追加

---
+++ portfolio/src/components/details/vue_todoapp.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class VueToDoListApp extends Component {
+    render() {
+        return (
+            <div>
+                <h3>ToDo管理アプリ</h3>
+                <h4>制作のきっかけ</h4>
+                <h4>使用言語・技術等</h4>
+                <p>JavaScript・Vue.js・Heroku</p>
+            </div>
+        )
+    }
+}
+
+export default VueToDoListApp;
